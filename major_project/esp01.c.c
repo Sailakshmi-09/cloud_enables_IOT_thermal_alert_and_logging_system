50657b6de0a5bd3d1498938f3cc3dc0df45f73f4
Sailakshmi-09 <kuppireddisai@gmail.com>
2026-07-22 23:04:06 +0530

    Update esp01.c.c

--- major_project/esp01.c.c
+++ major_project/esp01.c.c
@@ -12,20 +12,22 @@ extern u8 rsp;
 
 void esp01_connectAP(void)
 {
-        Write_CMD_LCD(0x01);
-        Write_CMD_LCD(0x80);
-        Write_str_LCD("AT");
+        Write_CMD_LCD(0x01);     //clear LCD
+        Write_CMD_LCD(0x80);     // move cursor to final line
+        Write_str_LCD("AT");      //display AT command on LCD
         delay_ms(1000);
-        UART0_Str("AT\r\n");
-        i=0;memset(buff,'\0',350);
-        while(i<4);
-        delay_ms(500);
-        buff[i] = '\0';
+        UART0_Str("AT\r\n");       //send AT command to ESP01
+        i=0;
+        memset(buff,'\0',350);     //clear response buffer    
+        while(i<4);                 //wait ffor response
+        delay_ms(500);             
+        buff[i] = '\0';             //NUll -terminate received string
         Write_CMD_LCD(0x01);
         Write_CMD_LCD(0x80);
-        Write_str_LCD(buff);
+        Write_str_LCD(buff);         //display ESP response
         delay_ms(2000);
-        if(strstr(buff,"OK"))
+        //check if ESP responded with ok
+if(strstr(buff,"OK"))
         {
                 Write_CMD_LCD(0xC0);
                 Write_str_LCD("OK");
@@ -38,6 +40,7 @@ void esp01_connectAP(void)
                 delay_ms(1000);
                 return;
         }
+        //disable command echo
          Write_CMD_LCD(0x01);
         Write_CMD_LCD(0x80);
         Write_str_LCD("ATE0");
@@ -51,7 +54,8 @@ void esp01_connectAP(void)
         Write_CMD_LCD(0x80);
         Write_str_LCD(buff);
         delay_ms(2000);
-        if(strstr(buff,"OK"))
+        //check response for ATEO command 
+if(strstr(buff,"OK"))
         {
                 Write_CMD_LCD(0xC0);
                 Write_str_LCD("OK");
@@ -64,6 +68,7 @@ void esp01_connectAP(void)
                 delay_ms(1000);
                 return;
         }
+        //set single connection mode
           Write_CMD_LCD(0x01);
         Write_CMD_LCD(0x80);
         Write_str_LCD("AT+CIPMUX");
@@ -77,7 +82,7 @@ void esp01_connectAP(void)
         Write_CMD_LCD(0x80);
         Write_str_LCD(buff);
         delay_ms(2000);
-        if(strstr(buff,"OK"))
+if(strstr(buff,"OK"))
         {
                 Write_CMD_LCD(0xC0);
                 Write_str_LCD("OK");
@@ -90,12 +95,14 @@ void esp01_connectAP(void)
                 delay_ms(1000);
                 return;
         }
+        //disconnect from any previously connected wi-fi network
           Write_CMD_LCD(0x01);
         Write_CMD_LCD(0x80);
         Write_str_LCD("AT+CWQAP");
         delay_ms(1000);
         UART0_Str("AT+CWQAP\r\n");
-        i=0;memset(buff,'\0',350);
+        i=0;
+        memset(buff,'\0',350);
         while(i<4);
         delay_ms(1500);
         buff[i] = '\0';
@@ -103,7 +110,7 @@ void esp01_connectAP(void)
         Write_CMD_LCD(0x80);
         Write_str_LCD(buff);
         delay_ms(2000);
-        if(strstr(buff,"OK"))
+if(strstr(buff,"OK"))
         {
                 Write_CMD_LCD(0xC0);
                 Write_str_LCD("OK");
@@ -116,6 +123,7 @@ void esp01_connectAP(void)
                 delay_ms(1000);
                 return;
         }
+        //connect to Wi-Fi access point
          Write_CMD_LCD(0x01);
         Write_CMD_LCD(0x80);
         Write_str_LCD("AT+CWJAP");
@@ -130,7 +138,8 @@ void esp01_connectAP(void)
         Write_CMD_LCD(0x80);
         Write_str_LCD(buff);
         delay_ms(2000);
-        if(strstr(buff,"WIFI CONNECTED"))
+        //check if wi-fi connection was successfull
+ if(strstr(buff,"WIFI CONNECTED"))
         {
                 Write_CMD_LCD(0xC0);
                 Write_str_LCD("OK");
@@ -147,6 +156,7 @@ void esp01_connectAP(void)
 }
 void esp01_sendToThingspeak(char val,char n)
 {
+        //start TCP connection with thingspeak server
         Write_CMD_LCD(0x01);
         Write_CMD_LCD(0x80);
         Write_str_LCD("AT+CIPSTART");
@@ -158,8 +168,9 @@ void esp01_sendToThingspeak(char val,char n)
         buff[i] = '\0';
         Write_CMD_LCD(0x01);
         Write_CMD_LCD(0x80);
-        Write_str_LCD(buff);
+Write_str_LCD(buff);
         delay_ms(2000);
+        //check if TCP connection is established
         if(strstr(buff,"CONNECT") || strstr(buff,"ALREADY CONNECTED"))
         {
                 Write_CMD_LCD(0xC0);
@@ -174,18 +185,14 @@ void esp01_sendToThingspeak(char val,char n)
                 i=0;memset(buff,'\0',350);
                 //while(buff[i] != '>');
                 delay_ms(500);
+                //send HTTP GET request to update field1
                 if(n==1)
                 {
                 //need to change the thingspeak write API key accordind to your channel
                 UART0_Str("GET /update?api_key=HCIPRXE9DS0L30TW&field1=");
                 }
-                /*else if(n==2)
-                {
-                //need to change the thingspeak write API key accordind to your channel
-                UART0_Str("GET /update?api_key=UQME4NXVTEHCFZB5&field2=");
-                } */
-                  UART0_Int(val);
-                UART0_Str("\r\n\r\n");
+ UART0_Int(val);     //send sensor value
+                UART0_Str("\r\n\r\n");  //End HTTP request
                 delay_ms(5000);
                 delay_ms(5000);
                 buff[i] = '\0';
@@ -193,6 +200,7 @@ void esp01_sendToThingspeak(char val,char n)
                 Write_CMD_LCD(0x80);
                 Write_str_LCD(buff);
                 delay_ms(2000);
+                //check if data was sent successfully
                 if(strstr(buff,"SEND OK"))
                 {
                         Write_CMD_LCD(0x01);
@@ -213,13 +221,14 @@ void esp01_sendToThingspeak(char val,char n)
                 Write_str_LCD("ERROR");
                 delay_ms(1000);
                 return;
-        }
+ }
 
 
 }
 void espo1_ReadfromThingspeak(void)
 {
-        u8 arr[350];
+        u8 arr[350];        //Temporary buffer for received data
+        //start TCP connection with thingspeak
         Write_CMD_LCD(0x01);
         Write_CMD_LCD(0x80);
         Write_str_LCD("AT+CIPSTART");
@@ -234,7 +243,8 @@ void espo1_ReadfromThingspeak(void)
         Write_str_LCD(buff);
         delay_ms(2000);
         Write_CMD_LCD(0x01);
-        if(strstr(buff,"CONNECT") || strstr(buff,"ALREADY CONNECTED"))
+        //check TCP connection
+ if(strstr(buff,"CONNECT") || strstr(buff,"ALREADY CONNECTED"))
         {
                 Write_CMD_LCD(0xC0);
                 Write_str_LCD("OK");
@@ -259,8 +269,9 @@ void espo1_ReadfromThingspeak(void)
                 Write_CMD_LCD(0x80);
                 Write_str_LCD(buff);
                 delay_ms(2000);
-                strcpy((char *)arr,buff);                                                                                                                   
-                if(strstr(buff,"SEND OK"))                                                                                                                                  {
+                strcpy((char *)arr,buff);    //copy received data
+                //check if data was received successfully    
+ if(strstr(buff,"SEND OK"))                                                                                                                                  {
                         Write_CMD_LCD(0x01);
                         Write_str_LCD("DATA UPDATED");
                         delay_ms(1000);
@@ -290,41 +301,47 @@ void extract_sp(u8 *p)
         u8 cnt=0,r[5],l=0;
         char *q;
         q=(char *)p;
+        //find the first occurance of "field1"
         q=strstr((const char *)q,"field1");
         //while(q=(strstr(q,"field1")))
         while(q)
         {
                 cnt++;
+                //process the second occurance of "field1"
                 if(cnt==2)
                 {
-                        //cnt=q-p;
                         //cnt+=7;
+                        //find':' character after field1
                         q=strchr((const char *)q,':');
                         //if(q=strchr((const char *)q,':'))
                         if(q)
                         {
-                                q=q+2;
+                                q=q+2; //move to the actual value
                         /*      Write_CMD_LCD(0x01);
                                 Write_str_LCD("extract2:");
                                 delay_ms(2000);
                                 Write_CMD_LCD(0x01);
                                 Write_str_LCD((char *)q);
                                 delay_ms(5000);*/
+//copy characters until closing quote
                                 for(;*q!='"';q++)
                                 {
                                         r[l++]=*q;
                                 }
-                                r[l]='\0';
+                                r[l]='\0';   //null-terminate extracted string
                         }
                         break;
                 }
                 q++;
                 q=strstr((const char *)q,"field1");
         }
+        //convert extracted string to integer
          rsp=atoi((const char *)r);
+         //display extracted value on LCD
         Write_CMD_LCD(0x01);
         Write_str_LCD("Remote sp: ");
         Write_int_LCD(rsp);
         delay_ms(3000);
         Write_CMD_LCD(0x01);
 }
+
