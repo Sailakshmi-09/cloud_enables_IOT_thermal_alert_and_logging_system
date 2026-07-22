f4ede01b16d985d6112d66edb1dc31dd8000c7b5
Sailakshmi-09 <kuppireddisai@gmail.com>
2026-07-22 22:54:17 +0530

    Update delay.h.h

--- major_project/delay.h.h
+++ major_project/delay.h.h
@@ -1,9 +1,15 @@
-                         /* delay.h */
-#ifndef __DELAY_H__
+                /* delay.h */
+#ifndef __DELAY_H__   // Prevents multiple inclusion of this header file
 #define __DELAY_H__
 
-#include "types.h"
+#include "types.h"       // Includes user-defined data types such as u32
+
+// Function declaration for millisecond delay
+// Parameter: dlyMS - delay duration in milliseconds
 void delay_ms(u32 dlyMS);
+
+// Function declaration for microsecond delay
+// Parameter: dlyUS - delay duration in microseconds
 void delay_us(u32 dlyUS);
 
 #endif
