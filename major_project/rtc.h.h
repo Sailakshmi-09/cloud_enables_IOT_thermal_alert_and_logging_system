5992cef1e155d1ac0e8aaef45a205e7679f4d98b
Sailakshmi-09 <kuppireddisai@gmail.com>
2026-07-22 23:16:44 +0530

    Enhance rtc.h.h with comments for clarity
    Added comments to clarify function declarations for RTC.

--- major_project/rtc.h.h
+++ major_project/rtc.h.h
@@ -1,7 +1,15 @@
-#ifndef __RTC_H__
+#ifndef __RTC_H__      // Prevents multiple inclusion of this header file
 #define __RTC_H__
 
+// Function declaration to initialize the Real Time Clock (RTC)
+// This function configures the RTC prescaler and enables the RTC
 void RTC_Init(void);
-void RTCSetTime(u32,u32,u32);
+
+// Function declaration to set the RTC time
+// Parameters:
+//   u32 - Hour value (0 to 23)
+//   u32 - Minute value (0 to 59)
+//   u32 - Second value (0 to 59)
+void RTCSetTime(u32, u32, u32);
 
 #endif
