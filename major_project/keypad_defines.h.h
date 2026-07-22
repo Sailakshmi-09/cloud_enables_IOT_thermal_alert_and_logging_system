9dbbcf98184fdb24a5efffb3dc9746e0204a7987
Sailakshmi-09 <kuppireddisai@gmail.com>
2026-07-22 23:11:45 +0530

    Enhance keypad_defines.h.h with comments
    Updated keypad pin definitions with comments for clarity.

--- major_project/keypad_defines.h.h
+++ major_project/keypad_defines.h.h
@@ -1,14 +1,32 @@
-#ifndef __KEYPAD_DEFINES_H__
+#ifndef __KEYPAD_DEFINES_H__   // Prevents multiple inclusion of this header file
 #define __KEYPAD_DEFINES_H__
 
-#define ROW0   16 //p1.16
+// ---------------- Keypad Row Pin Definitions ----------------
+
+// ROW0 is connected to P1.16
+#define ROW0   16
+
+// ROW1 is connected to P1.17
 #define ROW1   17
+
+// ROW2 is connected to P1.18
 #define ROW2   18
+
+// ROW3 is connected to P1.19
 #define ROW3   19
 
+// ---------------- Keypad Column Pin Definitions ----------------
+
+// COL0 is connected to P1.20
 #define COL0   20
+
+// COL1 is connected to P1.21
 #define COL1   21
+
+// COL2 is connected to P1.22
 #define COL2   22
+
+// COL3 is connected to P1.23
 #define COL3   23
 
 #endif
