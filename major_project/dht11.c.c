3406fae8f59c7c185607970ba53d6e656e897683
Sailakshmi-09 <kuppireddisai@gmail.com>
2026-07-22 22:56:33 +0530

    Update dht11.c.c

--- major_project/dht11.c.c
+++ major_project/dht11.c.c
@@ -1,39 +1,74 @@
-#define input 0
-#define output 1
+#define input 0       // Macro to configure a pin as input
+#define output 1      // Macro to configure a pin as output
 
-#define DHT11 4 //DHT11 data pin connected to P1.26
+#define DHT11 4       // DHT11 data pin connected to P0.4
 
-
+// Function to send a start request signal to the DHT11 sensor
 void dht11_request(void)
 {
-        WRITEBIT(IODIR0,DHT11,output);  // Configure DHT11 pin as output (P0.4 used here)
-        WRITEBIT(IOPIN0,DHT11,0); // Make DHT11 pin LOW for minimum 18 seconds
-        delay_ms(20);
-        WRITEBIT(IOPIN0,DHT11,1); // Make DHT11 pin HIGH and wait for response
+    // Configure DHT11 pin as output
+    WRITEBIT(IODIR0, DHT11, output);
+
+    // Pull the data line LOW for at least 18 ms
+    // This signals the DHT11 to start communication
+    WRITEBIT(IOPIN0, DHT11, 0);
+
+    // Keep the line LOW for 20 ms
+    delay_ms(20);
+
+    // Pull the data line HIGH
+    // The DHT11 will respond after this
+    WRITEBIT(IOPIN0, DHT11, 1);
 }
 
+// Function to wait for the DHT11 response signal
 void dht11_response(void)
 {
-        WRITEBIT(IODIR0,DHT11,input);   // Configure DHT11 pin as input
-        //delay_ms(100);
-        while(READBIT(IOPIN0,DHT11) == 1); // Wait till response is HIGH
-        while(READBIT(IOPIN0,DHT11) == 0); // Wait till response is LOW
-        while(READBIT(IOPIN0,DHT11) == 1); // Wait till response is HIGH  & This is end of response
+    // Configure DHT11 pin as input to receive data
+    WRITEBIT(IODIR0, DHT11, input);
+// Wait until the DHT11 pulls the line LOW
+    while(READBIT(IOPIN0, DHT11) == 1);
+
+    // Wait until the DHT11 pulls the line HIGH
+    while(READBIT(IOPIN0, DHT11) == 0);
+
+    // Wait until the DHT11 completes its response signal
+    while(READBIT(IOPIN0, DHT11) == 1);
 }
 
+// Function to read one byte (8 bits) of data from the DHT11
 unsigned char dht11_data(void)
 {
-        unsigned char count;
-        unsigned char data = 0;
-        for(count = 0; count<8 ; count++)       // 8 bits of data
+    unsigned char count;
+    unsigned char data = 0;   // Variable to store received byte
+
+    // Loop to read 8 bits
+    for(count = 0; count < 8; count++)
+    {
+        // Wait until the line goes HIGH (start of bit transmission)
+        while(READBIT(IOPIN0, DHT11) == 0);
+
+        // Wait for 30 microseconds
+        // DHT11 sends:
+        //   ~26-28 us HIGH for bit 0
+        //   ~70 us HIGH for bit 1
+        delay_us(30);
+// Read the pin state after 30 us
+        if (READBIT(IOPIN0, DHT11))
         {
-                while(READBIT(IOPIN0,DHT11) == 0);      // Wait till response is LOW
-                delay_us(30);   // delay greater than 24 usec
-                if (READBIT(IOPIN0,DHT11)) // If response is HIGH, 1 is received
-                        data = ( (data<<1) | 0x01 );
-                else    // If response is LOW, 0 is received
-                        data = (data<<1);
-                while(READBIT(IOPIN0,DHT11));   // Wait till response is HIGH (happens if 1 is received)
+            // If still HIGH, it is a bit '1'
+            data = ((data << 1) | 0x01);
         }
-        return data;
+        else
+        {
+            // If LOW, it is a bit '0'
+            data = (data << 1);
+        }
+
+        // Wait until the HIGH signal ends before reading next bit
+        while(READBIT(IOPIN0, DHT11));
+    }
+
+    // Return the complete 8-bit data byte
+    return data;
 }
